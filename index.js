3ba9638e0bb77491aadb3e27e943cb0527e91424
tatiananonatadasilvabarros-oss <tatiananonatadasilvabarros@gmail.com>
2026-06-25 17:20:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -26,3 +26,10 @@ async function connectToWhatsApp() {
 }
 
 connectToWhatsApp()
+// Mantém o Render vivo
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => res.send('Bot online'))
+app.listen(PORT, () => console.log(`Servidor fake na porta ${PORT}`))
