308a9507692c062a0d7bed0780713cdd95f4a15d
tatiananonatadasilvabarros-oss <tatiananonatadasilvabarros@gmail.com>
2026-06-25 17:09:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-const { default: makeWASocket, useMultiFileAuthState, DisconnectReason } = require('@whiskeysockets/baileys')
+const { default: makeWASocket, useMultiFileAuthState } = require('@whiskeysockets/baileys')
 
 async function connectToWhatsApp() {
     const { state, saveCreds } = await useMultiFileAuthState('auth_info_baileys')
     
     const sock = makeWASocket({
         auth: state,
-        printQRInTerminal: true, // ESSA LINHA É OBRIGATÓRIA NO RENDER
+        printQRInTerminal: false, // Desliga QR
         browser: ['Ubuntu', 'Chrome', '20.0.04']
     })
 
+   5511995507551
+    if (!sock.authState.creds.registered) {
+        const phoneNumber = '55XXXXXXXXXXX' // COLOCA SEU NÚMERO AQUI COM DDI 55 + DDD
+        const code = await sock.requestPairingCode(phoneNumber)
+        console.log(`SEU CÓDIGO DE PAREAMENTO: ${code}`)
+    }
+
     sock.ev.on('connection.update', (update) => {
-        const { connection, lastDisconnect } = update
-        
-        if(connection === 'close') {
-            const shouldReconnect = lastDisconnect?.error?.output?.statusCode !== DisconnectReason.loggedOut
-            if(shouldReconnect) {
-                connectToWhatsApp()
-            }
-        } else if(connection === 'open') {
-            console.log('CONECTADO! BOT ONLINE!')
+        const { connection } = update
+        if(connection === 'open') {
+            console.log('BOT CONECTADO! PODE FECHAR O LOG')
         }
     })
 
